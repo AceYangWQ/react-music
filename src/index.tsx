12cca5caa9ccac3ac438c7855fdb6163fad6f9ea
yangwuqi <1120817870@qq.com>
2024-03-05 20:44:59 +0800

    add router

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'normalize.css'
+import './assets/css/index.less'
+import { HashRouter } from 'react-router-dom'
+
 import App from '@/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 )
